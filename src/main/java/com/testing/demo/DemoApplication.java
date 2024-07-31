e20a8504534732dfcbf2d2f4c7f4b01a97142761
aison cabusay <cabusayaison@gmail.com>
2024-07-31 21:29:14 +0800

    for testing only

---
+++ src/main/java/com/testing/demo/DemoApplication.java
@@ -0,0 +1,9 @@
+package com.testing.demo;
+
+
+public class DemoApplication {
+    public static void main(String[] args) {
+        // Print "Hello, World!" to the console
+        System.out.println("Hello, World!");
+    }
+}
---
+++ src/test/java/com/testing/demo/DemoApplicationTests.java
@@ -0,0 +1,11 @@
+package com.testing.demo;
+
+import org.junit.jupiter.api.Test;
+
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
