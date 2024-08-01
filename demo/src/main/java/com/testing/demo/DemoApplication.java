b62285b1b521be7d41924979539646f9c4e968bf
aison cabusay <cabusayaison@gmail.com>
2024-08-01 18:26:53 +0800

    Java testing API

--- demo/src/main/java/com/testing/demo/DemoApplication.java
+++
@@ -1,9 +0,0 @@
-package com.testing.demo;
-
-
-public class DemoApplication {
-    public static void main(String[] args) {
-        // Print "Hello, World!" to the console
-        System.out.println("Hello, World!");
-    }
-}
